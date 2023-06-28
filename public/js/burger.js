2b56896e8802eec5c9f625bcc98f6584c5a2359f
KalimuenGoat <polodu165@gmail.com>
2023-06-28 09:28:38 +0200

    Configuration du Burger Menu

---
+++ public/js/burger.js
@@ -0,0 +1,11 @@
+console.log("hello");
+
+function afficherBloc() {
+  var bloc = document.getElementById("bloc");
+  bloc.style.display = "block";
+}
+
+function fermerBloc() {
+  var bloc = document.getElementById("bloc");
+  bloc.style.display = "none";
+}
