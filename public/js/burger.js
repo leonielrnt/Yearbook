b1def5f727f732621163a743f43b7982eba1bc77
KalimuenGoat <polodu165@gmail.com>
2023-06-29 14:02:48 +0200

    Ajout du fond gris quand on ouvre le menu burger

--- public/js/burger.js
+++ public/js/burger.js
@@ -6,6 +6,8 @@ burgermenuClose.addEventListener("click", () => closeMenu());
 
 function displayMenu() {
   burgermenu.style.display = "block";
+  document.body.classList.add("backgroundburger");
+  document.body.style.overflow = "hidden";
   setTimeout(() => {
     burgermenu.style.opacity = "1";
     burgermenu.style.transform = "translateX(0)";
@@ -17,5 +19,35 @@ function closeMenu() {
   burgermenu.style.transform = "translateX(100%)";
   setTimeout(() => {
     burgermenu.style.display = "none";
+    document.body.classList.remove("backgroundburger");
+    document.body.style.overflow = "auto";
   }, 300);
+}
+
+document.body.addEventListener("touchstart", handleTouchStart, false);
+document.body.addEventListener("touchmove", handleTouchMove, false);
+document.body.addEventListener("touchend", handleTouchEnd, false);
+
+let startX = 0;
+let dist = 0;
+
+function handleTouchStart(event) {
+  startX = event.touches[0].clientX;
+  dist = 0;
+}
+
+function handleTouchMove(event) {
+  if (startX) {
+    const currentX = event.touches[0].clientX;
+    dist = currentX - startX;
+    burgermenu.style.transform = `translateX(${dist}px)`;
+  }
+}
+
+function handleTouchEnd() {
+  if (startX && dist < -50) {
+    closeMenu();
+  }
+  startX = 0;
+  dist = 0;
 }
